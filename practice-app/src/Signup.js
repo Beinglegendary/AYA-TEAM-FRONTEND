55130f84bf80b98d254069af0cf5469a9347557b
Beinglegendary <legendarycolins@yahoo.com>
2023-03-06 09:52:54 +0100

    signup2

--- practice-app/src/Signup.js
+++ practice-app/src/Signup.js
@@ -5,44 +5,57 @@ import imageHolder from "./images/Image-holder.svg";
 function Signup() {
   return (
     <div className="signup-container">
+      {" "}
       <div className="signup-info">
-        <h2>Create an account</h2>
-        <p>Welcome to view</p>
+        {" "}
+        <h2>Create an account</h2> <p>Welcome to view</p>{" "}
         <form>
+          {" "}
           <label>
-            First Name: <input type="text" name="firstName" />
-          </label>
+            {" "}
+            First Name: <input type="text" name="firstName" />{" "}
+          </label>{" "}
           <label>
-            Last Name: <input type="text" name="lastName" />
-          </label>
+            {" "}
+            Last Name: <input type="text" name="lastName" />{" "}
+          </label>{" "}
           <label>
-            Work Email: <input type="email" name="workEmail" />
-          </label>
+            {" "}
+            Work Email: <input type="email" name="workEmail" />{" "}
+          </label>{" "}
           <label>
-            Password: <input type="password" name="password" />
-          </label>
+            {" "}
+            Password: <input type="password" name="password" />{" "}
+          </label>{" "}
           <div className="buttons">
+            {" "}
             <button className="back-Button" type="button">
-              Back
-            </button>
+              {" "}
+              Back{" "}
+            </button>{" "}
             <button className="submit-Button" type="submit">
-              Next
-            </button>
-          </div>
+              {" "}
+              Next{" "}
+            </button>{" "}
+          </div>{" "}
           <p className="last_text">
+            {" "}
             I would rather{" "}
             <span>
-              <a href="#">Sign up</a>
-            </span>
-          </p>
+              {" "}
+              <a href="#">Sign up</a>{" "}
+            </span>{" "}
+          </p>{" "}
           <a class="text-back" href="#">
-            &lt; Back home
-          </a>
-        </form>
-      </div>
+            {" "}
+            &lt; Back home{" "}
+          </a>{" "}
+        </form>{" "}
+      </div>{" "}
       <div className="signup-image">
-        <img src={imageHolder} alt="Signup" />
-      </div>
+        {" "}
+        <img src={imageHolder} alt="Signup" />{" "}
+      </div>{" "}
     </div>
   );
 }
